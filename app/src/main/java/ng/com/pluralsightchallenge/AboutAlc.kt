2efa25cd906e49d365f7708032e98ec9f5ab5c4d
Maaruf Dauda <mkdauda.work@gmail.com>
2019-07-15 17:39:27 +0100

    Begin About ALC

---
+++ app/src/main/java/ng/com/pluralsightchallenge/AboutAlc.kt
@@ -0,0 +1,12 @@
+package ng.com.pluralsightchallenge
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutAlc : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_alc)
+    }
+}
